9c72e00b9031f8d11509854007e7cab9298388b1
Reid Watson <j.watsonreid@gmail.com>
2020-09-07 22:09:20 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -17,6 +17,7 @@ def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
   ary = [
+    {},
     {name:  "Joelle van Dyne", occupation: "Radio Personality" },
     {name:  "Pat Monteseian", occupation: "Staff"},
     {name:  "Kate Gompert", occupation: "None"},
